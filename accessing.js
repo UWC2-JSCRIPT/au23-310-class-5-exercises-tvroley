5a1cbb70538a7589b8a062fa254e95ae7126080c
Theodore Roley <tvroley@uw.edu>
2023-11-07 19:37:29 -0800

    changed sun elements to orange

--- accessing.js
+++ accessing.js
@@ -3,5 +3,9 @@ const weatherHead = document.getElementById('weather-head');
 weatherHead.innerText = "February 10 Weather Forecast, Seattle";
 
 // Change the styling of every element with class "sun" to set the color to "orange"
+const sunElement = document.getElementsByClassName('sun');
+for(let element of sunElement) {
+    element.style.color = 'orange';
+}
 
 // Change the class of the second <li> from to "sun" to "cloudy"
