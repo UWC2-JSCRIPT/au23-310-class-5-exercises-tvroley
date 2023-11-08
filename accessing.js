38fb385aec12ba392d40d284e6c99c1d502c8fd3
Theodore Roley <tvroley@uw.edu>
2023-11-07 19:25:18 -0800

    changed weather header text

--- accessing.js
+++ accessing.js
@@ -1,4 +1,6 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+const weatherHead = document.getElementById('weather-head');
+weatherHead.innerText = "February 10 Weather Forecast, Seattle";
 
 // Change the styling of every element with class "sun" to set the color to "orange"
 
