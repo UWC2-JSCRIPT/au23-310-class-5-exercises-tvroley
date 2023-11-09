feb8726265c515f70bf09fe67fb0b1c61c8d5038
Theodore Roley <tvroley@uw.edu>
2023-11-08 22:46:49 -0800

    traversed to body and logged it to the console

--- traversing.js
+++ traversing.js
@@ -11,6 +11,19 @@ while(currentChild) {
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 const ul = document.querySelector('ul');
+let currentParent = ul.parentElement;
+let bodyEl = null;
+let foundBody = false;
+while(currentParent && !foundBody) {
+    if(currentParent.tagName === 'BODY') {
+        bodyEl = currentParent;
+        foundBody = true;
+    }
+    currentParent = currentParent.parentElement;
+}
+if(bodyEl) {
+    console.log(bodyEl);
+}
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
