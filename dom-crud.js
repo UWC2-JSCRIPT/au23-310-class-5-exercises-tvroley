ef50045d1dfd45795a8109f01d34840b224ea990
Theodore Roley <tvroley@uw.edu>
2023-11-08 20:02:03 -0800

    new <a> element after last <p>

--- dom-crud.js
+++ dom-crud.js
@@ -1,6 +1,18 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
-
+let mainEl = document.getElementsByTagName('main')[0];
+let currentChild = mainEl.firstElementChild;
+let lastP = null;
+while(currentChild) {
+    if(currentChild.tagName === 'P') {
+        lastP = currentChild;
+    }
+    currentChild = currentChild.nextElementSibling;
+}
+const aEl = document.createElement('a');
+aEl.id = 'cta';
+aEl.innerText = 'Buy Now!';
+lastP.insertAdjacentElement('afterend', aEl);
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
