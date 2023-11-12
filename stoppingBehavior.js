cc20161ff924fa2c560c804903a15877b8e7a54e
Theodore Roley <tvroley@uw.edu>
2023-11-12 15:22:36 -0800

    clicking link shows alert

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,11 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+const moreInfoEl = document.getElementById('more-info');
+moreInfoEl.addEventListener('click', (e) => {
+  e.preventDefault();
+  alert(`Here's some info`);
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
