ccc90ae27904c42a0f4c691dcc184147404dee1b
Theodore Roley <tvroley@uw.edu>
2023-11-12 14:21:57 -0800

    where's the pointer

--- wheresThePointer.js
+++ wheresThePointer.js
@@ -1,3 +1,7 @@
 // Attach one listener that will detect clicks on any of the <td>
 // elements.  Should update that element's innerHTML to be the
 // x, y coordinates of the mouse at the time of the click
+const tbodyEl = document.getElementsByTagName('tbody')[0];
+tbodyEl.addEventListener('click', (e) => {
+    e.target.innerText = `${e.clientX}, ${e.clientY}`;
+});
